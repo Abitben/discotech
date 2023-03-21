5874f28e6e8d545b18525f6532467ab581c610c2
Benjamin Dupaquier <dupaquier.benjamin@gmail.com>
2023-03-21 23:11:12 +0100

    add a route for kitui demo

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :albums
   devise_for :users
   resources :users
+
+  get '/KitUI', to: 'demo#show'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
