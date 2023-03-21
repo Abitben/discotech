d576f7473fbd85656c31554fc122d65f7fc5d109
Benjamin Dupaquier <dupaquier.benjamin@gmail.com>
2023-03-21 19:29:08 +0100

    scaffold albums

---
+++ db/migrate/20230321182739_create_albums.rb
@@ -0,0 +1,12 @@
+class CreateAlbums < ActiveRecord::Migration[7.0]
+  def change
+    create_table :albums do |t|
+      t.string :title
+      t.string :artist
+      t.integer :year
+      t.decimal :price, precision: 5, scale: 2
+
+      t.timestamps
+    end
+  end
+end
