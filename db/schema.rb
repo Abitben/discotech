9fc8d13574c7d1fb18dfd99d88d2bbf03ef67a4b
Benjamin Dupaquier <dupaquier.benjamin@gmail.com>
2023-03-27 15:41:00 +0200

    migrate is okay

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,89 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_23_202147) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "ads", force: :cascade do |t|
+    t.decimal "price", precision: 7, scale: 2
+    t.text "description"
+    t.integer "status", default: 0
+    t.bigint "copy_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["copy_id"], name: "index_ads_on_copy_id"
+  end
+
+  create_table "albums", force: :cascade do |t|
+    t.integer "release_id"
+    t.string "title"
+    t.string "artist"
+    t.integer "year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "cartlines", force: :cascade do |t|
+    t.bigint "ad_id", null: false
+    t.bigint "cart_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ad_id"], name: "index_cartlines_on_ad_id"
+    t.index ["cart_id"], name: "index_cartlines_on_cart_id"
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "copies", force: :cascade do |t|
+    t.bigint "album_id", null: false
+    t.bigint "user_id", null: false
+    t.integer "media_condition", default: 0
+    t.integer "sleeve_condition", default: 0
+    t.integer "status", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["album_id"], name: "index_copies_on_album_id"
+    t.index ["user_id"], name: "index_copies_on_user_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "cart_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_orders_on_cart_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "address"
+    t.string "zip_code"
+    t.string "city_name"
+    t.string "country"
+    t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.boolean "is_admin"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "ads", "copies"
+  add_foreign_key "cartlines", "ads"
+  add_foreign_key "cartlines", "carts"
+  add_foreign_key "copies", "albums"
+  add_foreign_key "copies", "users"
+  add_foreign_key "orders", "carts"
+  add_foreign_key "orders", "users"
 end
