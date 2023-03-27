e6cc47341decfe1442ff8a023334eeeab6a6e5d6
Benjamin Dupaquier <dupaquier.benjamin@gmail.com>
2023-03-27 16:27:40 +0200

    restore accent in db. To do: map for front

--- app/models/copy.rb
+++ app/models/copy.rb
@@ -1,27 +1,27 @@
 class Copy < ApplicationRecord
   enum :status, { whished: 0, owned: 1}
-  enum :sleeve_condition, { neuf: 0,
-    comme_neuf: 1,
-    tres_bon_etat: 2,
-    bon_etat: 3,
-    etat_acceptable: 4,
-    mauvais_etat: 5,
-    tres_mauvais_etat: 6,
-    dechiree: 7,
-    abimee: 8,
-    manquante: 9}, prefix: true
+  enum :sleeve_condition, { Neuf: 0,
+    Comme_neuf: 1,
+    Très_bon_état: 2,
+    Bon_état: 3,
+    Etat_acceptable: 4,
+    Mauvais_état: 5,
+    Très_mauvais_état: 6,
+    Dechirée: 7,
+    Abimée: 8,
+    Manquante: 9}, prefix: true
 
   enum :media_condition, {
-    neuf: 0,
-    comme_neuf: 1,
-    tres_bon_etat: 2,
-    bon_etat: 3,
-    etat_acceptable: 4,
-    mauvais_etat: 5,
-    tres_mauvais_etat: 6,
-    raye: 7,
-    saute: 8,
-    endommage: 9 }, prefix: true
+    Neuf: 0,
+    Comme_neuf: 1,
+    Très_bon_état: 2,
+    Bon_état: 3,
+    Etat_acceptable: 4,
+    Mauvais_état: 5,
+    Tres_mauvais_état: 6,
+    Raye: 7,
+    Saute: 8,
+    Endommagé: 9 }, prefix: true
   
   belongs_to :album
   belongs_to :user
