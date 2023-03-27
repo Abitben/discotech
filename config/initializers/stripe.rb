ecc5183260573fca0b3bcf78191f53249d0b806f
Benjamin Dupaquier <dupaquier.benjamin@gmail.com>
2023-03-27 22:45:43 +0200

    test with stripe, order is okay

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -2,4 +2,5 @@ Rails.configuration.stripe = {
   :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
   :secret_key      => ENV['STRIPE_API_SECRET']
 }
+
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
