fa373d2f2fe535b918a4668b42b07148c18daa14
Benjamin Dupaquier <dupaquier.benjamin@gmail.com>
2023-03-27 15:20:08 +0200

    remove relation between albums and cartlines and carts

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,9 +1,6 @@
 class Album < ApplicationRecord
     has_many :copies
     has_many :users, through: :copies
-
-    has_many :cartlines
-    has_many :carts, through: :cartlines
-
+    
     validates :title,  :artist, :year, :price, presence: true
 end
