f7c2b76b7c5970d36b31e1fab71ae9866ad0e949
Florian <florian.sasso@outlook.fr>
2023-03-27 16:36:06 +0200

    Install ActiveStorage / Add upload and display avatar to _sidebar - to do : resize image

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_one_attached :avatar #do |attachable|
+    #attachable.variant :thumb, resize_to_limit: [100, 100]
+  #end
   # after_create :welcome_send
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
