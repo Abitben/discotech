cb184bf55eb0eba1dfced2a67d068c9b6b0cb58d
Benjamin Dupaquier <dupaquier.benjamin@gmail.com>
2023-03-27 18:32:30 +0200

    correcting relation in cartlines

--- app/controllers/cartlines_controller.rb
+++ app/controllers/cartlines_controller.rb
@@ -2,18 +2,18 @@ class CartlinesController < ApplicationController
 
 def create
     # Find associated album and current cart
-    chosen_album = Album.find(params[:album_id])
+    chosen_album = Ad.find(params[:ad_id])
     current_cart = @current_cart
 
     # If cart already has this album then find the relevant cartline and iterate quantity otherwise create a new cartline for this album
-    if current_cart.albums.include?(chosen_album)
+    if current_cart.ad.include?(chosen_album)
       # Find the cartline with the chosen_album
-      @cartline = current_cart.cartlines.find_by(:album_id => chosen_album)
+      @cartline = current_cart.cartlines.find_by(:ad_id => chosen_album)
       # Iterate the cartline's quantity by one
     else
       @cartline = Cartline.new
       @cartline.cart = current_cart
-      @cartline.album = chosen_album
+      @cartline.ad = chosen_album
     end
   
     # Save and redirect to cart show path
@@ -22,7 +22,6 @@ def create
 end
 
   def destroy
-    puts "coucou"
     @cartline = Cartline.find(params[:id])
     @cartline.destroy
     redirect_to cart_path(@current_cart)
@@ -30,7 +29,7 @@ end
   
 private
     def cartline_params
-        params.require(:cartline).permit(:album_id, :cart_id)
+        params.require(:cartline).permit(:ad_id, :cart_id)
     end
 
 end
