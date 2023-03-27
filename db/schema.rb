0d28153a3b798bc416dbfd4e1e86ad4300b40b1a
Benjamin Dupaquier <dupaquier.benjamin@gmail.com>
2023-03-27 14:40:43 +0200

    migration copies ok. to do: test relations between tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_21_182739) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_21_184337) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_21_182739) do
     t.decimal "price", precision: 5, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "copies", force: :cascade do |t|
+    t.bigint "album_id", null: false
+    t.bigint "user_id", null: false
+    t.string "media_condition"
+    t.string "sleeve_condition"
+    t.integer "status", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["album_id"], name: "index_copies_on_album_id"
+    t.index ["user_id"], name: "index_copies_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -43,4 +55,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_21_182739) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "copies", "albums"
+  add_foreign_key "copies", "users"
 end
