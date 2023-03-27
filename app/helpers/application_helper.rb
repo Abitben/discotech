b380e12963f3c3503d05b223518d80c34d6c0433
Benjamin Dupaquier <dupaquier.benjamin@gmail.com>
2023-03-28 01:25:33 +0200

    add a method find_user_copies for collection, wishlist, orderscontrolles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,21 +1,26 @@
 module ApplicationHelper
   def is_admin?
     if user_signed_in?
-      if current_user.is_admin == true
-        return true
-      else
-        redirect_to root_path
-      end
+      return true if current_user.is_admin == true
+
+      redirect_to root_path
+
     else
       redirect_to root_path
     end
   end
 
   def profile_completed?
-    exceptions = ["reset_password_token", "reset_password_sent_at", "remember_created_at", "is_admin"] 
-    if current_user.attributes.except(*exceptions).any? { |_, attributes| attributes.nil? }  
-      redirect_to edit_user_path(current_user)
+    exceptions = %w[reset_password_token reset_password_sent_at remember_created_at is_admin]
+    return unless current_user.attributes.except(*exceptions).any? { |_, attributes| attributes.nil? }
+
+    redirect_to edit_user_path(current_user)
   end
 
+  def find_user_copies
+    user = User.find(params[:user_id])
+    user_copies = Copy.where(user_id: user.id)
+    return user_copies
   end
+  
 end
