2c2c1fe558dbc322b32987102e2b7d9522d5e59e
Benjamin Dupaquier <dupaquier.benjamin@gmail.com>
2023-03-27 16:38:25 +0200

    correcting relations between albums and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :collections, dependent: :destroy
-  has_many :albums, through: :collections
+  has_many :copies, dependent: :destroy
+  has_many :albums, through: :copies
 
   has_many :orders
 
