4a63621a6d5f03be03bce461ea87c81e2330c536
Benjamin Dupaquier <dupaquier.benjamin@gmail.com>
2023-03-27 13:05:17 +0200

    add a column release_id to albums

--- db/migrate/20230321182739_create_albums.rb
+++ db/migrate/20230321182739_create_albums.rb
@@ -1,6 +1,7 @@
 class CreateAlbums < ActiveRecord::Migration[7.0]
   def change
     create_table :albums do |t|
+      t.integer :release_id
       t.string :title
       t.string :artist
       t.integer :year
