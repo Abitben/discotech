be52f5ece53ad16e7ab0c8ca8a1e9f18667c71ad
Benjamin Dupaquier <dupaquier.benjamin@gmail.com>
2023-03-27 14:09:48 +0200

    add a column status, for owned or whished

--- db/migrate/20230321184337_create_copies.rb
+++ db/migrate/20230321184337_create_copies.rb
@@ -3,6 +3,9 @@ class CreateCollections < ActiveRecord::Migration[7.0]
     create_table :copies do |t|
       t.references :album, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
+      t.string :media_condition
+      t.string :sleeve_condition
+      t.integer :status, default: 0
 
       t.timestamps
     end
