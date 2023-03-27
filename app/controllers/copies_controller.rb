953c35b32ae483a984e183dd5e6cc47dae2a648e
Benjamin Dupaquier <dupaquier.benjamin@gmail.com>
2023-03-27 19:38:30 +0200

    change details to make the show page to run

--- app/controllers/copies_controller.rb
+++ app/controllers/copies_controller.rb
@@ -38,3 +38,4 @@ class CopiesController < ApplicationController
   end
 
 end
+
--- spec/requests/copies_spec.rb
+++ spec/requests/copies_spec.rb
@@ -2,45 +2,6 @@ require 'rails_helper'
 
 RSpec.describe "Copies", type: :request do
   describe "GET /index" do
-    it "returns http success" do
-      get "/copies/index"
-      expect(response).to have_http_status(:success)
-    end
+    pending "add some examples (or delete) #{__FILE__}"
   end
-
-  describe "GET /show" do
-    it "returns http success" do
-      get "/copies/show"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /edit" do
-    it "returns http success" do
-      get "/copies/edit"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /create" do
-    it "returns http success" do
-      get "/copies/create"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /update" do
-    it "returns http success" do
-      get "/copies/update"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/copies/destroy"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
