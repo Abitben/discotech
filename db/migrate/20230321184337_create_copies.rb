1a0cd9a9570b5889df248ac18c7bdda0021b17cc
Benjamin Dupaquier <dupaquier.benjamin@gmail.com>
2023-03-27 12:58:08 +0200

    rename table collections to copies

--- db/migrate/20230321184337_create_copies.rb
+++ db/migrate/20230321184337_create_copies.rb
@@ -1,6 +1,6 @@
 class CreateCollections < ActiveRecord::Migration[7.0]
   def change
-    create_table :collections do |t|
+    create_table :copies do |t|
       t.references :album, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
 
