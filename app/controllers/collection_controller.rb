b81000397310b6040f73391f840dfeb2bb6b0b79
Benjamin Dupaquier <dupaquier.benjamin@gmail.com>
2023-03-28 01:27:48 +0200

    use find_user_copies in collection, and wishlist

--- app/controllers/collection_controller.rb
+++ app/controllers/collection_controller.rb
@@ -1,7 +1,5 @@
 class CollectionController < ApplicationController
   def index
-    @user = User.find(params[:user_id])
-    user_copies = Copy.where(user_id: @user.id)
-    @owned = user_copies.owned 
+    @owned = find_user_copies.owned 
   end
 end
--- app/controllers/wishlist_controller.rb
+++ app/controllers/wishlist_controller.rb
@@ -1,7 +1,5 @@
 class WishlistController < ApplicationController
   def index
-    @user = User.find(params[:user_id])
-    user_copies = Copy.where(user_id: @user.id)
-    @wished = user_copies.wished 
+    @wished = find_user_copies.wished 
   end
 end
