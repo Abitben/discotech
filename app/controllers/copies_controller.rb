781f7a2f1d11c90a2afcebc8983088bcc2b9ea66
Benjamin Dupaquier <dupaquier.benjamin@gmail.com>
2023-03-27 19:13:25 +0200

    modifiying controller

--- app/controllers/copies_controller.rb
+++ app/controllers/copies_controller.rb
@@ -1,5 +1,8 @@
 class CopiesController < ApplicationController
+  before_action :set_copy, only: %i[ show edit update destroy ]
+
   def index
+    @collection = Copy.where(user_id: current_user)
   end
 
   def show
@@ -9,11 +12,29 @@ class CopiesController < ApplicationController
   end
 
   def create
+    @copy = Copy.new(ad_params)
   end
 
   def update
+      if @copy.update(ad_params)
+        redirect_to copy_url(@copy), notice: "Ad was successfully updated."
+      else
+        render :edit, status: :unprocessable_entity
+      end
   end
 
   def destroy
+    @copy.destroy
   end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_copy
+    @copy = Copy.find(params[:id])
+  end
+
+  def ad_params
+    params.require(:copy).permit(:album_id, :user_id, :media_condition, :sleeve_condition, :status)
+  end
+
 end
