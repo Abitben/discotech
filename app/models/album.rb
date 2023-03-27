3a2472d90ec310eaa688033603a1411edc423b20
Benjamin Dupaquier <dupaquier.benjamin@gmail.com>
2023-03-27 15:49:46 +0200

    remove price from mandatory entries for album

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,5 +2,5 @@ class Album < ApplicationRecord
     has_many :copies
     has_many :users, through: :copies
     
-    validates :title,  :artist, :year, :price, presence: true
+    validates :title,  :artist, :year, presence: true
 end
