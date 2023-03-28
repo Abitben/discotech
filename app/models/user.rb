f4463d510eec669521a78d750763a99dfc086437
Perzisso <117381013+Perzisso@users.noreply.github.com>
2023-03-28 15:03:27 +0200

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,7 @@ class User < ApplicationRecord
     # Regexp extracted from https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
     return if password.blank? || password =~ /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])/
 
-    errors.add :password, 'Complexity requirement not met. Please use: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
+    errors.add :password, 'Veuillez inclure : 1 lettre capitale, 1 lettre minuscule, 1 chiffre and 1 caractère spécial'
   end
 
 
