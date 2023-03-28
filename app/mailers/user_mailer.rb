db83874d8a3faf6690aa987c787b5849c365096d
Ulrichvoitier <ulrichvoitier@yahoo.fr>
2023-03-28 14:41:08 +0200

    modified mail adress and add config action in development

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-    default from: 'no-reply@discotech.fr'
+    default from: 'ulrichvoitier@yahoo.fr'
 
   def welcome_email(user)
     #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
