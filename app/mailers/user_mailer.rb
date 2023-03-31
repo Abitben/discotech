10d1d301895c03e8338f55c8454ca653d06bb60e
Benjamin Dupaquier <dupaquier.benjamin@gmail.com>
2023-03-31 19:21:35 +0200

    correct a error unexpected end

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -25,7 +25,7 @@ class UserMailer < ApplicationMailer
     # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant le destinataire et sujet.
     mail(to: @user.email, subject: 'Bienvenue chez nous !')
   end
-end
+
 
 
   def confirmation_email(user, order)
